f3a09b36af207a526972ce867e702be6c319ef34
tomat <xxlennygamerxx@gmail.com>
2024-06-20 19:08:33 -0700

    Update license headers is remaining update files since I'll be borrowing from OpenAsar.

--- discord/src/update/module-updater.ts
+++ discord/src/update/module-updater.ts
@@ -1,4 +1,5 @@
 /* Copyright (C) 2024  Tomat et al.
+ * Copyright (C) 2022  GooseNest
  *
  * This program is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Affero General Public License as published by the
